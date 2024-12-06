b36704c320e7ba956a3410b9401b5dfb359d3521
SansGrinder <jiahua.zhang@student.keystoneacademy.cn>
2024-12-06 12:57:07 +0800

    Added a Runner.java, simplified usefulFunctions.java, renamed Main.java to OnLaunch.java.

---
+++ src/main/java/org/example/CrashExport.java
@@ -0,0 +1,60 @@
+package org.example;
+import java.io.*;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+public class CrashExport {
+    public static String formattedTime="";
+    public static void generateCrashReport(int programRunState, Exception e){
+        System.out.println("generateCrashReport has been called, with a programRunState of "+programRunState+" and the exception being "+e);
+        String stackTrace;
+        StringWriter sw = new StringWriter();
+        PrintWriter pw = new PrintWriter(sw);
+        e.printStackTrace(pw);
+        stackTrace = sw.toString();
+        String programRunPhase = switch (programRunState) {
+            case 0 -> "Before mode selection";
+            case 1 -> "Encryption";
+            case 2 -> "After Encryption";
+            case 3 -> "PDF Encryption";
+            case 4 -> "After PDF Encryption";
+            case 5 -> "Decryption";
+            case 6 -> "After decryption";
+            default -> "Unable to fetch program run state";
+        };
+        saveCrashLog(System.getProperty("user.home")+"/Downloads","Program run state: "+programRunPhase+"\nException:\n"+stackTrace);
+        if (!Runner.programEndedExpectedly){
+            CrashEmail email=new CrashEmail(
+                    System.getProperty("user.home").split("/")[2]+" got an "+e+"!",
+                    "Program run state: "+programRunPhase+"\nException:\n"+stackTrace
+            );
+            email.sendAnEmail();
+        }
+    }
+    private static void saveCrashLog(String filePath, String content) {
+        // Get the current time with seconds
+        LocalDateTime currentTime = LocalDateTime.now();
+        // Define a custom date and time format
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd-HH-mm-ss");
+        // Format the current time using the defined format
+        formattedTime = currentTime.format(formatter);
+        // Create a File
+        File file = new File(filePath, "crash-log-at-"+formattedTime+".log");
+        try {
+            if (file.createNewFile()) {
+                System.out.println("File created successfully!");
+            } else {
+                System.out.println("Failed to create the file!");
+            }
+        } catch (IOException e) {
+            System.out.println("An error occurred while creating the file: " + e.getMessage());
+        }
+        // Now we write the content of the file
+        try (FileWriter writer = new FileWriter(file)){
+            writer.write(content);
+            System.out.println("File written successfully!");
+        } catch (IOException e) {
+            System.out.println("An error occurred while overriding the file: " + e.getMessage());
+        }
+    }
+}
--- src/main/java/org/example/GUIs.java
+++ src/main/java/org/example/GUIs.java
@@ -1,9 +1,5 @@
 package org.example;
-
-import javax.swing.ImageIcon;
 import javax.swing.JOptionPane;
-import java.util.Objects;
-
 public class GUIs { // Sorted by order of execution
     public static void programCrashed(Exception e){
         msgPopUp("The program has crashed due to an error!\nA crash report will be saved to your Downloads folder! Send Gordon the crash log so he can fix it!","Oh No...","error");
@@ -33,9 +29,6 @@ public class GUIs { // Sorted by order of execution
                     JOptionPane.showMessageDialog(null, text, title, JOptionPane.PLAIN_MESSAGE);
         }
     }
-    public static int confirmPopUp(String text, String title){
-        return JOptionPane.showConfirmDialog(null, text, title, 0,0,null);
-    }
     public static int optionPopUp(String text, String title, String[] options){
         return JOptionPane.showOptionDialog(null, text, title, JOptionPane.YES_NO_OPTION, JOptionPane.INFORMATION_MESSAGE, null, options, "");
     }
@@ -45,5 +38,4 @@ public class GUIs { // Sorted by order of execution
     public static String textPopUp(String text, Object placeHolderText){
         return JOptionPane.showInputDialog(text,placeHolderText);
     }
-    public static String pullDownMenu(String text,String title,String[] options){ return (String)JOptionPane.showInputDialog(null,text,title,0,null,options,"Yes!");}
 }
